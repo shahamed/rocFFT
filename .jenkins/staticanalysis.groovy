cbbfbc5197aebbd589f737a8f54ce9b1071b4f14
Sam Wu <samwu103@amd.com>
2023-03-13 14:31:09 -0600

    Documentation update for Read the Docs
    Standardize rocFFT documentation builds to use the rocm-docs-core template (#1036)
    
    * Update readthedocs to follow pitchfork layout
    
    * Only build html files in rtd yaml format
    
    move images to data folder
    
    build doxygen files
    
    * Fix doxygen enums in api.rst
    
    remove rocm.jpg
    
    * Remove pdf and epub doc formats
    
    Keep only htmlzip doc formats
    
    * Add doc build steps
    
    * Add docBin folder to gitignore
    
    Update README doc build instructions
    
    * Remove all docBin files
    
    Remove old doc scripts and steps using them in jenkin static analysis job (#1043)
    
    Fix HTML report error (#1048)
    
    * remove publishHTML command as docs are no longer built with bash scripts
    
    html docs now built on read the docs as htmlzip format
    
    Add dependabot config and pin rocm-docs-core (#1062)
    
    fix documentation (#1069)
    
    use correct ref for doxygenenum directive
    
    Update documentation requirements (#1095)
    
    Update documentation dependencies via requirements file
    
    Make separate section for Design docs in Table of Contents (#1103)
    
    rocm-docs-core==0.13.4

--- .jenkins/staticanalysis.groovy
+++ .jenkins/staticanalysis.groovy
@@ -13,13 +13,6 @@ def runCompileCommand(platform, project, jobName, boolean debug=false)
 {
     project.paths.construct_build_prefix()
 
-    def command = """#!/usr/bin/env bash
-            set -x
-            ${project.paths.project_build_prefix}/docs/run_doc.sh
-            """
-
-    platform.runCommand(this, command)
-
     def yapfCommand = """#!/usr/bin/env bash
                          set -x
                          cd ${project.paths.project_build_prefix}
@@ -30,14 +23,6 @@ def runCompileCommand(platform, project, jobName, boolean debug=false)
                       """
 
     platform.runCommand(this, yapfCommand)
-    
-    publishHTML([allowMissing: false,
-                alwaysLinkToLastBuild: false,
-                keepAll: false,
-                reportDir: "${project.paths.project_build_prefix}/docs/source/_build/html",
-                reportFiles: "index.html",
-                reportName: "Documentation",
-                reportTitles: "Documentation"])
 }
 
 def runCI =
