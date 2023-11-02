274fe996d7e6474967f31ae528541751aaf650d6
Steve Leung <Steve.Leung@amd.com>
2023-11-02 10:04:14 -0600

    allow manual multi-device tests
    * rocfft-test: add manual_devices arg to allow manual multi-device tests
    
    * fft_params: print field/brick config in str()

--- clients/tests/multi_device_test.cpp
+++ clients/tests/multi_device_test.cpp
@@ -37,99 +37,43 @@ std::vector<fft_params> param_generator_multi_gpu()
     if(deviceCount < 2)
         return {};
 
-    std::vector<fft_params> all_params
-        = param_generator_complex(multi_gpu_sizes,
-                                  precision_range_sp_dp,
-                                  {1, 10},
-                                  stride_generator({{1}}),
-                                  stride_generator({{1}}),
-                                  {{0, 0}},
-                                  {{0, 0}},
-                                  {fft_placement_inplace, fft_placement_notinplace},
-                                  false);
+    auto params_complex = param_generator_complex(multi_gpu_sizes,
+                                                  precision_range_sp_dp,
+                                                  {1, 10},
+                                                  stride_generator({{1}}),
+                                                  stride_generator({{1}}),
+                                                  {{0, 0}},
+                                                  {{0, 0}},
+                                                  {fft_placement_inplace, fft_placement_notinplace},
+                                                  false);
 
-    auto all_params_real = param_generator_real(multi_gpu_sizes,
-                                                precision_range_sp_dp,
-                                                {1, 10},
-                                                stride_generator({{1}}),
-                                                stride_generator({{1}}),
-                                                {{0, 0}},
-                                                {{0, 0}},
-                                                {fft_placement_notinplace},
-                                                false);
-    std::copy(all_params_real.begin(), all_params_real.end(), std::back_inserter(all_params));
-    all_params_real.clear();
+    auto params_real = param_generator_real(multi_gpu_sizes,
+                                            precision_range_sp_dp,
+                                            {1, 10},
+                                            stride_generator({{1}}),
+                                            stride_generator({{1}}),
+                                            {{0, 0}},
+                                            {{0, 0}},
+                                            {fft_placement_notinplace},
+                                            false);
 
-    for(auto& params : all_params)
-    {
-        // split up the slowest FFT dimension among the available
-        // devices
-        size_t islowLen = params.length.front();
-        size_t oslowLen = params.olength().front();
-        if(islowLen < static_cast<unsigned int>(deviceCount)
-           || oslowLen < static_cast<unsigned int>(deviceCount))
-            continue;
+    std::vector<fft_params> all_params;
 
-        // add input and output fields
-        auto& ifield = params.ifields.emplace_back();
-        auto& ofield = params.ofields.emplace_back();
-
-        for(int i = 0; i < deviceCount; ++i)
+    auto distribute_params = [&all_params, deviceCount](const std::vector<fft_params>& params) {
+        for(auto& p : params)
         {
-            // start at origin
-            std::vector<size_t> ifield_lower(params.length.size());
-            std::vector<size_t> ifield_upper(params.length.size());
-            std::vector<size_t> ofield_lower(params.length.size());
-            std::vector<size_t> ofield_upper(params.length.size());
+            // distribute both input and output
+            auto p_both = p;
+            p_both.distribute_input(deviceCount);
+            p_both.distribute_output(deviceCount);
 
-            // note: slowest FFT dim is index 1 in these coordinates
-            ifield_lower[0] = islowLen / deviceCount * i;
-            ofield_lower[0] = oslowLen / deviceCount * i;
-            // last brick needs to include the whole slow len
-            if(i == deviceCount - 1)
-            {
-                ifield_upper[0] = islowLen;
-                ofield_upper[0] = oslowLen;
-            }
-            else
-            {
-                ifield_upper[0] = std::min(islowLen, ifield_lower[0] + islowLen / deviceCount);
-                ofield_upper[0] = std::min(oslowLen, ofield_lower[0] + oslowLen / deviceCount);
-            }
-
-            for(unsigned int upperDim = 1; upperDim < params.length.size(); ++upperDim)
-            {
-                ifield_upper[upperDim] = params.length[upperDim];
-                ofield_upper[upperDim] = params.olength()[upperDim];
-            }
-
-            // field coordinates also need to include batch
-            ifield_lower.insert(ifield_lower.begin(), 0);
-            ofield_lower.insert(ofield_lower.begin(), 0);
-            ifield_upper.insert(ifield_upper.begin(), params.nbatch);
-            ofield_upper.insert(ofield_upper.begin(), params.nbatch);
-
-            // bricks have contiguous strides
-            size_t              brick_idist = 1;
-            size_t              brick_odist = 1;
-            std::vector<size_t> brick_istride(ifield_lower.size());
-            std::vector<size_t> brick_ostride(ofield_lower.size());
-            for(size_t i = 0; i < ifield_lower.size(); ++i)
-            {
-                // fill strides from fastest to slowest
-                *(brick_istride.rbegin() + i) = brick_idist;
-                brick_idist *= *(ifield_upper.rbegin() + i) - *(ifield_lower.rbegin() + i);
-
-                *(brick_ostride.rbegin() + i) = brick_odist;
-                brick_odist *= *(ofield_upper.rbegin() + i) - *(ofield_lower.rbegin() + i);
-            }
-
-            ifield.bricks.push_back(
-                fft_params::fft_brick{ifield_lower, ifield_upper, brick_istride, i});
-            ofield.bricks.push_back(
-                fft_params::fft_brick{ofield_lower, ofield_upper, brick_ostride, i});
+            all_params.emplace_back(std::move(p_both));
         }
-    }
+    };
+
+    distribute_params(params_complex);
+    distribute_params(params_real);
+
     return all_params;
 }
 
