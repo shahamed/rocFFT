e15e31d5123e8f56fd8b5f03d08cd3b5f466fa05
Sam Wu <sam.wu2@amd.com>
2024-01-02 15:00:46 -0700

    Standardize documentation for ReadtheDocs (#1221)

--- docs/conf.py
+++ docs/conf.py
@@ -1,10 +1,28 @@
+import re
+
 from rocm_docs import ROCmDocs
 
-external_projects_current_project = "rocfft"
+with open('../CMakeLists.txt', encoding='utf-8') as f:
+    match = re.search(r'.*\bset \( VERSION_STRING\s+\"?([0-9.]+)[^0-9.]+', f.read())
+    if not match:
+        raise ValueError("VERSION not found!")
+    version_number = match[1]
+left_nav_title = f"rocFFT {version_number} Documentation"
 
-docs_core = ROCmDocs("rocFFT Documentation")
-docs_core.run_doxygen()
+# for PDF output on Read the Docs
+project = "rocFFT Documentation"
+author = "Advanced Micro Devices, Inc."
+copyright = "Copyright (c) 2024 Advanced Micro Devices, Inc. All rights reserved."
+version = version_number
+release = version_number
+
+external_toc_path = "./sphinx/_toc.yml"
+
+docs_core = ROCmDocs(left_nav_title)
+docs_core.run_doxygen(doxygen_root="doxygen", doxygen_path="doxygen/xml")
 docs_core.setup()
+
+external_projects_current_project = "rocfft"
 
 for sphinx_var in ROCmDocs.SPHINX_VARS:
     globals()[sphinx_var] = getattr(docs_core, sphinx_var)
